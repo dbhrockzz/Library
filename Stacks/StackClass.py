9dea9b8bca8f6b9ae5afc3d65086b9e22e3f267b
dbhrockzz <dbhrockzz1@gmail.com>
2017-03-03 23:46:56 +0530

    Stack Class Added

---
+++ Stacks/StackClass.py
@@ -0,0 +1,23 @@
+class stack:
+    
+    def __init__(self):
+        self.top = -1
+        self.st = []
+    
+    def push(self,n):
+        self.st.append(n)
+        self.top+=1
+    
+    def pop(self):
+        if self.top==-1:
+            return
+        self.top-=1
+        return self.st.pop()
+    
+    def peek(self):
+        return self.st[self.top]
+    
+    def isEmpty(self):
+        if self.top==-1:
+            return True
+        return False
